7591376fabd2910162abb216373f4da2500c8550
liunics <xonleo@163.com>
2024-02-22 14:57:42 +0800

    Site updated: 2024-02-22 14:57:42

--- attach/ann/js/tb.js
+++ attach/ann/js/tb.js
@@ -5,7 +5,7 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
   var abbr = document.getElementById('abbr').value;
   if (person != "" && tag != "" && abbr != "") {
     if (person == "男神" && tag == "以上都是" && abbr == "CIRP")
-      alert('情人节快乐，你也看出来了，我不是一个浪漫的人，但是也许偶尔会给你带来surprise，like this。');
+      alert('情人节快乐！你也看出来了，我不是一个浪漫的人，但是也许偶尔会给你带来surprise，like this。');
     else
       alert("答错了！\n温馨提示：如果实在答不对，向对方承认自己是憨憨可以要到正确答案哦。");
   }
